4b3ced121908e53171bedb55bc638319f546cf51
Mürsel SEN <murselsen_35@hotmail.com>
2025-05-07 23:16:35 +0300

    Add Contact component with basic structure and styling

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div className="contact">
+            <div className='name'>
+                <span>*</span> <p>Rosie Simpson</p>
+            </div>
+            <div className='name'>
+                <span>*</span> <p>459-12-56</p>
+            </div>
+        </div>
+    )
+}
+
+export default Contact;
