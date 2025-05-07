c154d5b0b8f0f4886712edf77e0b8051618eb418
Mürsel SEN <murselsen_35@hotmail.com>
2025-05-08 00:01:21 +0300

    Remove unused user and phone call icons; update Contact component to use imported icons

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import Css from './Contact.module.css';
+import userIcon from './icon/user.png';
+import phoneIcon from './icon/phone-call.png';
 const Contact = () => {
     const handleDelete = (e) => {
-        console.log('Delete contact');
+        console.log('Delete contact', e);
     }
     return (
         <div className={Css.Contact}>
             <div className={Css.Info}>
                 <div className={Css.InfoItem}>
-                    <img src='
-                    icon/user.png' width="16" height="16" title='user' /> <p>Rosie Simpson</p>
+                    <img src={userIcon} width="16" height="16" title='user' /> <p>Rosie Simpson</p>
                 </div>
                 <div className={Css.InfoItem}>
-                    <img src='
-                    icon/phone-call.png' width="16" height="16" title='phone' /> <p>459-12-56</p>
+                    <img src={phoneIcon} width="16" height="16" title='phone' /> <p>459-12-56</p>
                 </div>
             </div>
             <div className={Css.Buttons}>
